f8e667da059dc10eaaf9cebf98e89992dce271a2
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2023-03-11 20:28:25 +0600

    Update main.cpp

---
+++ week-4/Meherun Nesa Enta/Important Pages on CodeChef/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x==1 && y ==0 )
+	cout<<"https://www.codechef.com/contests";
+	else if( x==0)
+	cout<<"https://www.codechef.com/practice";
+	else
+	cout<<"https://discuss.codechef.com";
+	return 0;
+}
