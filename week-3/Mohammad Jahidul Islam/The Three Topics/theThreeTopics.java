0e446573b9e768fdf36f23228a4ccd49990ebebb
Jahidul Islam <jahidulzaid@gmail.com>
2023-03-12 13:06:26 +0600

    backup updated

---
+++ week-3/Mohammad Jahidul Islam/The Three Topics/theThreeTopics.java
@@ -0,0 +1,32 @@
+/* package codechef; // don't place package name! */
+//@jahidulZaid
+
+import java.util.*;
+
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		
+			int a=sc.nextInt();
+			int b=sc.nextInt();
+			int c=sc.nextInt();
+			int x=sc.nextInt();
+			
+			
+
+			if( x == a || x == b || x == c)
+			{
+				System.out.println("YES");
+			}
+			else
+			{
+				System.out.println("NO");
+			}
+            sc.close();
+	}
+}
