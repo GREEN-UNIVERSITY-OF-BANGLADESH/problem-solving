ba2cf2e11ac4aa5e66179c1fde44b7907bf7a6da
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2023-03-21 20:22:48 +0600

    Add files via upload

---
+++ week-5/Md. Maruf Sarker/Chess Format/main.cpp
@@ -0,0 +1,50 @@
+// Written by: Md. Maruf Sarker
+// Platform: Codechef
+// Problem: https://www.codechef.com/problems/CHSFORMT
+// Language: cpp / go / c / python
+// Date: 2022-11-27
+// Difficulty Rating: 844
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define pi 3.141592653589793238462
+#define mod 1000000007
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+
+int read(){static int x; cin >> x; return x;}
+ll readll(){static ll x; cin >> x; return x;}
+double readd(){static double x; cin >> x; return x;}
+ld readld(){static ld x; cin >> x; return x;}
+string reads(){static string x; cin >> x; return x;}
+char readc(){static char x; cin >> x; return x;}
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);  cout.tie(NULL);  cout.precision(2);
+
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int a, b;
+        cin >> a >> b;
+
+        if((a + b) < 3) cout << 1 << endl;
+        else if((a + b) >= 3 and (a + b) <= 10) cout << 2 << endl;
+        else if((a + b) >= 11 and (a + b) <= 60) cout << 3 << endl;
+        else cout << 4 << endl;
+    }
+    return 0;
+}
---
+++ week-5/Md. Maruf Sarker/Problem Category/main.cpp
@@ -0,0 +1,49 @@
+// Written by: Md. Maruf Sarker
+// Platform: Codechef
+// Problem: https://www.codechef.com/problems/PROBCAT
+// Language: cpp / go / c / python
+// Date: 2022-11-27
+// Difficulty Rating: 860
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+#define pb push_back
+#define pi 3.141592653589793238462
+#define mod 1000000007
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<ll>>
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define sz(x) (int)x.size()
+
+int read(){static int x; cin >> x; return x;}
+ll readll(){static ll x; cin >> x; return x;}
+double readd(){static double x; cin >> x; return x;}
+ld readld(){static ld x; cin >> x; return x;}
+string reads(){static string x; cin >> x; return x;}
+char readc(){static char x; cin >> x; return x;}
+
+int main() {
+ios_base::sync_with_stdio(false);
+cin.tie(NULL);  cout.tie(NULL);  cout.precision(2);
+
+    int t;
+    cin >> t;
+    
+    while(t--) {
+        int n;
+        cin >> n;
+
+        if(n >= 1 and n < 100) cout << "Easy" << endl;
+        else if(n >= 100 and n < 200) cout << "Medium" << endl;
+        else cout << "Hard" << endl;
+    }
+    return 0;
+}
