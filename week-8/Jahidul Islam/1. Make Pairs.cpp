e43493d750d6a7ee2506c010f3c593b91f89f59d
Md. Maruf Sarker <78826405+mdmarufsarker@users.noreply.github.com>
2023-05-22 01:36:03 +0600

    Week 8

--- week-8/Jahidul Islam/1. Make Pairs.cpp
+++ week-8/Jahidul Islam/1. Make Pairs.cpp
@@ -1,7 +1,7 @@
     // Writter      : @JahidulZaid
     // Problem Link : https://www.codechef.com/problems/PAIREQ
     // Language     : c++
-    // Date         : 17/5/2023
+    // Date         : 19/5/2023
     // Submission   : 
     #include <bits/stdc++.h>
     using namespace std;
@@ -13,6 +13,23 @@
         int t;
         cin >> t;
         cin.ignore();
+        while (t--)
+        {
+            int n;
+            cin>>n;
+            int a[n];
+            int f[1001];
+            for (int i = 0; i < n; i++)
+            {
+                cin>>a[i];
+                f[a[i]]++;
+            }
+            int s = sizeof(a)/ sizeof(a[0]);
+            sort(a, a+s);
+            cout<< n-a[1001] <<endl;
+            
+        }
+        
         
    
         return 0;
