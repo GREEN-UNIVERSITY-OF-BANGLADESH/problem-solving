0041b15745dfbc27c0c0c7f240e011e885fce6f4
Tanvir Ahmed <132837418+tanvirahmed1732@users.noreply.github.com>
2023-05-22 20:53:10 +0600

    01

--- Week 10/Tanvir Ahmed 😎/ICPC_Balloons.java
+++ Week 10/Tanvir Ahmed 😎/ICPC_Balloons.java
@@ -11,19 +11,19 @@ public class ICPC_Balloons {
             for (int i = 0; i < n; i++) {
                 ar[i] = sc.nextInt();
             }
-            int x = 0;
+            int x = 0; // this variable for check balloons for problems 1 through 7 are tied or not.
             int j;
             for (j = 0; j < n; j++) {
                 if(x<7){
                     if(ar[j]==1 || ar[j]==2 || ar[j]==3 || ar[j]==4 ||
-                            ar[j]==5 || ar[j]==6 || ar[j]==7){
-                        x++;
+                            ar[j]==5 || ar[j]==6 || ar[j]==7){ // if there is any balloon for problem 1 to 7 then x++ 
+                        x++; //and the loop will be continue if I don't collect all 7 balloons. (codition  x<7)
                     }
                 }
                 else
-                    break;
+                    break; // if I find all 7 balloons then there is no need to continue the loop. so, i use break here.
             }
-            System.out.println(j);
+            System.out.println(j); // I print j here because j'th index is the final index where i find the last balloon to complete all 1 to 7 balloon.
         }
     }
 }
